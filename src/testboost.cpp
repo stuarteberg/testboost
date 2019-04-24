b088d18cfa65444bdd74a39d65abc77ae6019c22
Stuart Berg <bergs@janelia.hhmi.org>
2019-04-24 14:52:58 -0400

    comment

--- src/testboost.cpp
+++ src/testboost.cpp
@@ -8,5 +8,10 @@ char const* greet(int x)
 BOOST_PYTHON_MODULE(testboost)
 {
     using namespace boost::python;
+
+    // Interestingly, the segfault goes away if we remove the default arg here (=0).
+    // But that syntax isn't wrong.  It merely exposes the build problem.
+    // (There are probably other ways to expose our build problems,
+    // this is the first one I've found.)
     def("greet", greet, (arg("x")=0));
 }
