0d21fdd36a24fc88ed97b6e108543be07bcc6f16
Stuart Berg <bergs@janelia.hhmi.org>
2019-04-24 14:50:00 -0400

    Remove commented-out lines

--- src/testboost.cpp
+++ src/testboost.cpp
@@ -5,16 +5,8 @@ char const* greet(int x)
    return "hello, world";
 }
 
-
-//class C {
-//public:
-//    C(int x) {};
-//};
-
 BOOST_PYTHON_MODULE(testboost)
 {
     using namespace boost::python;
     def("greet", greet, (arg("x")=0));
-
-    //class_<C>("C", init<int>((arg("x"))));
 }
